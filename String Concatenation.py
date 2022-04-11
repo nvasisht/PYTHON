fb9c79de1a23adbecab2b4734bdefef4e07effdc
n.vasisht <n.vasisht8@gmail.com>
2022-04-11 14:13:49 +0300

    Added to string file

---
+++ String Concatenation.py
@@ -0,0 +1,15 @@
+
+#To concatenate or combine ---- two strings can use (+) operator
+
+a = "Hello"
+b = "concatenate"
+
+c = a+b
+print(c)
+
+# To add a space between the text 
+a = "Hello"
+b = "concatenate"
+
+c = a +" "+ b
+print(c)
